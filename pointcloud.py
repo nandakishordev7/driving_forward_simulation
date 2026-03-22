7a04c151f804e02c76ac4a4e1f3e5f3e30b86f48
Nandakishor <developernandakishor@gmail.com>
2026-03-22 22:34:37 +0530

    Refactor depth scaling and clean up comments

--- pointcloud.py
+++ pointcloud.py
@@ -15,7 +15,7 @@ def depth_to_pointcloud(depth, image):
 
     ys, xs = np.meshgrid(np.arange(h), np.arange(w), indexing='ij')
 
-    z = depth * 0.02   # 🔥 increase scale for better spread
+    z = depth * 0.02  
 
     X = (xs - cx) * z / fx
     Y = (ys - cy) * z / fy
@@ -23,7 +23,6 @@ def depth_to_pointcloud(depth, image):
     points = np.stack((X, Y, z), axis=-1).reshape(-1, 3)
     colors = image.reshape(-1, 3)
 
-    # 🔥 Remove invalid points
     mask = z.reshape(-1) > 0
     points = points[mask]
     colors = colors[mask]
